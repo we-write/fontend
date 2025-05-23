22e096e4ca76ccf9f56a11bba13fedac79e7946e
myeongheonhong <ruddyh7076@gmail.com>
2025-05-23 10:33:19 +0900

    style: 필요하지 않은 부분 주석 제거

--- src/app/social/page.tsx
+++ src/app/social/page.tsx
@@ -1,30 +1,9 @@
-import WriteListGrid from './writeListGrid';
+import { getSocialList } from '@/api/social/api';
+import SocialListGrid from './SocialListGrid';
 
 const Page = async () => {
-  // const queryClient = new QueryClient();
-
-  // await queryClient.prefetchQuery({
-  //   queryKey: [QUERY_KEY.SOCIAL],
-  //   queryFn: getSocialList,
-  // });
-
-  return (
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    <div>
-      {/* <PageHeader/> */}
-
-      <div>
-        <>
-          {/* <CategoryTabBar /> */}
-          {/* <button>모임만들기</button> */}
-        </>
-        {/* <FilterBar /> */}
-      </div>
-
-      <WriteListGrid />
-    </div>
-    // </HydrationBoundary>
-  );
+  const initialSocialList = await getSocialList();
+  return <SocialListGrid initialSocialList={initialSocialList} />;
 };
 
 export default Page;
